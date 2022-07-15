2faa737145af48852b5a05f35e5ff51f5214d761
Moonhyeseong <moonhs97@gmail.com>
2022-07-15 13:07:21 +0900

    노션 api 문제해결중

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,7 @@
 import Layout from '../components/layout';
 import Head from 'next/head';
+import { TOKEN, DATABASE_ID } from '../config';
+
 const Projects = () => {
   return (
     <Layout>
@@ -15,3 +17,29 @@ const Projects = () => {
 ``;
 
 export default Projects;
+
+//빌드타임에 호출
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
+  const projects = await res.json();
+  // console.log(result);
+
+  const projectsNames = projects.results.map((project) => {
+    console.log(project.properties);
+  });
+
+  return {
+    props: {},
+  };
+}
